99154bae04701c03107d7c5328bb6314cd848b21
pradeeprdev <pradeeprawat1717@gmail.com>
2025-04-25 19:47:07 +0530

    push

--- nextjs-version/src/app/page.tsx
+++ nextjs-version/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import HomePage from "@/pages/HomePage";
+import HomePage from "./HomePage";
 
 export default function Home() {
   return (

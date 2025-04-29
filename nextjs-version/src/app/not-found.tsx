f4c06a8b011a340ced8b5caad962ede510eaedc6
pradeeprdev <pradeeprawat1717@gmail.com>
2025-04-29 14:52:05 +0530

    push code

---
+++ nextjs-version/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+      <div className="text-center mt-20">
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-4 text-lg text-gray-600">
+          Oops! The page you are looking for does not exist.
+        </p>
+      </div>
+    );
+  }
+  
